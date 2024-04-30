1e03874f4a1c0ecbc50e7f6c0e8cb3e4b75c2660
IamEzio <anshumanmaurya111@gmail.com>
2024-05-01 03:02:05 +0530

    removed debug code

--- src/validation_checks/p2sh.rs
+++ src/validation_checks/p2sh.rs
@@ -1,29 +1,29 @@
-use std::vec;
+use std::vec; // Import the standard `vec` module.
 
-use hex;
-use log::info;
+use hex; // Import the `hex` crate for hexadecimal encoding and decoding.
+use log::info; // Import the `info` function from the `log` crate for logging purposes.
 
-use crate::validation_checks::hash160;
-use crate::validation_checks::op_checkmultisig;
-use crate::validation_checks::op_checksig;
+use crate::validation_checks::hash160; // Import the `hash160` function from the `validation_checks` module.
+use crate::validation_checks::op_checkmultisig; // Import the `op_checkmultisig` function from the `validation_checks` module.
+use crate::validation_checks::op_checksig; // Import the `op_checksig` function from the `validation_checks` module.
 
-use crate::{error::Result, transaction::Transaction};
+use crate::{error::Result, transaction::Transaction}; // Import the `Result` type and `Transaction` struct from the crate.
 
-// IMPUT VERIFICATION FOR P2SH
 pub fn input_verification_p2sh(tx_input_index: usize, tx: Transaction) -> Result<bool> {
-    let scriptpubkey_asm = tx.vin[tx_input_index].prevout.scriptpubkey_asm.clone();
+    let scriptpubkey_asm = tx.vin[tx_input_index].prevout.scriptpubkey_asm.clone(); // Clone the script pubkey ASM.
 
-    let witness = match tx.vin[tx_input_index].witness.clone() {
+    let witness = match tx.vin[tx_input_index].witness.clone() { // Match and clone the witness data.
         Some(value) => value,
-        None => Vec::new(),
+        None => Vec::new(), // If witness data is None, create an empty Vec.
     };
 
-    let scriptsig_asm = match tx.vin[tx_input_index].scriptsig_asm.clone() {
+    let scriptsig_asm = match tx.vin[tx_input_index].scriptsig_asm.clone() { // Match and clone the scriptsig ASM.
         Some(value) => value,
         None => {
-            return Ok(false);
+            return Ok(false); // If scriptsig ASM is None, return false.
         }
     };
+
 
     let inner_redeemscript_asm = match tx.vin[tx_input_index].inner_redeemscript_asm.clone() {
         Some(value) => value,
@@ -42,7 +42,6 @@ pub fn input_verification_p2sh(tx_input_index: usize, tx: Transaction) -> Result
     )?)
 }
 
-// SCRIPT EXECUTION
 fn script_execution_p2sh(
     scriptpubkey_asm: String,
     witness: Vec<String>,
@@ -54,7 +53,6 @@ fn script_execution_p2sh(
     let mut script_result: bool = false;
     let input_type: &str;
 
-    // DEFINING THE SCRIPT TYPE OF THE VERIFYING INPUT
     if witness.len() == 0 {
         input_type = "NON_SEGWIT";
     } else if witness.len() == 2 {
@@ -65,7 +63,6 @@ fn script_execution_p2sh(
 
     let mut stack = Vec::new();
 
-    // EXECUTING SCRIPT SIG ASM
     let scriptsig_asm_opcodes: Vec<&str> = scriptsig_asm.split_whitespace().collect();
     for opcode_index in 0..scriptsig_asm_opcodes.len() {
         let is_pushbytes = scriptsig_asm_opcodes[opcode_index].starts_with("OP_PUSHBYTES");
@@ -73,23 +70,19 @@ fn script_execution_p2sh(
 
         match scriptsig_asm_opcodes[opcode_index] {
             "OP_0" => {
-                // Push an empty array of bytes in the stack
                 stack.push(vec![0 as u8]);
             }
             _ if is_pushbytes => {
-                // PUSHING SIGNATURES OR REDEEM SCRIPT IN SCRIPT SIG ASM
                 stack.push(hex::decode(&scriptsig_asm_opcodes[opcode_index + 1])?);
             }
 
             _ if is_pushdata => {
-                // REDEEM SCRIPT
                 stack.push(hex::decode(&scriptsig_asm_opcodes[opcode_index + 1])?);
             }
             _ => continue,
         }
     }
 
-    // EXECUTING SCRIPT PUB KEY
     let scriptpubkey_asm_opcodes: Vec<&str> = scriptpubkey_asm.split_whitespace().collect();
     for opcode_index in 0..scriptpubkey_asm_opcodes.len() {
         match scriptpubkey_asm_opcodes[opcode_index] {
@@ -116,10 +109,8 @@ fn script_execution_p2sh(
     }
 
 
-    // EXECUTING THE INNER REDEEM SCRIPT AS PER THE SCRIPT TYPE
     if input_type == "NON_SEGWIT" {
 
-        // EXECUTE INNER REDEEM SCRIPT ASM
         let inner_redeemscript_asm_opcodes: Vec<&str> =
             inner_redeemscript_asm.split_whitespace().collect();
 
@@ -138,7 +129,6 @@ fn script_execution_p2sh(
                 "OP_PUSHNUM_4" => stack.push(vec![4u8]),
 
                 _ if is_pushbytes => {
-                    // PUSHING THE PUBLIC KEYS, SIGNATURES IN THE STACK
                     stack.push(
                         hex::decode(&inner_redeemscript_asm_opcodes[opcode_index + 1])
                             .expect("DECODING: FAILED"),
@@ -146,7 +136,6 @@ fn script_execution_p2sh(
                 }
 
                 _ if is_pushdata => {
-                    // PUSHING DATA IN THE STACK
                     stack.push(
                         hex::decode(&inner_redeemscript_asm_opcodes[opcode_index + 1])
                             .expect("DECODING: FAILED"),
@@ -154,17 +143,14 @@ fn script_execution_p2sh(
                 }
 
                 "OP_0" => {
-                    // Push an empty array of bytes in the stack
                     stack.push(vec![0 as u8]);
                 }
 
                 "OP_CSV" => {
-                    // CHECK THE PREV PUSHED DATA WITH THE SEQUENCE OF THE INPUT, LATER
                     continue;
                 }
 
                 "OP_DROP" => {
-                    // POPS THE TOP ELEMENT OF THE STACK
                     stack.pop().expect("SATCK UNDERFLOW: OP_DROP");
                 }
 
@@ -180,10 +166,8 @@ fn script_execution_p2sh(
                     let b = stack.pop().expect("STACK UNDERFLOW: OP_EQUALVERIFY");
 
                     if a == b {
-                        // println!("REDEEM EQUAL: SUCCESSFULL");
                         script_result = true;
                     } else {
-                        // println!("REDEEM EQUAL: FAILED");
                         return Ok(false);
                     }
                 }
@@ -192,24 +176,20 @@ fn script_execution_p2sh(
                     let result = op_checksig(&mut stack, tx.clone(), tx_input_index, input_type)?;
 
                     if result == false {
-                        // println!("OP_CHECKSIGVERIFY: FAILED");
                         return Ok(false);
                     }
                 }
 
                 "OP_DEPTH" => {
-                    // PUSH THE EXISTING LENGTH OF THE STACK
                     stack.push(vec![0 as u8]);
                 }
 
                 "OP_CHECKSIG" => {
-                    // IMPLEMENT CHECKSIG OPCODE
                     script_result =
                         op_checksig(&mut stack, tx.clone(), tx_input_index, input_type)?;
                 }
 
                 "OP_CHECKMULTISIG" => {
-                    // IMPLEMENT CHECKMULTISIG OPCODE
                     script_result =
                         op_checkmultisig(&mut stack, tx.clone(), tx_input_index, input_type)?;
                 }
@@ -219,24 +199,13 @@ fn script_execution_p2sh(
     }
 
     if input_type == "P2SH-P2WPKH" {
-        // EXECUTE WITNESS
-
-        // PUSH SIGNATURE
         stack.push(hex::decode(&witness[0]).expect("DECODING: FAILED"));
 
-        // PUSH PUBLIC KEY
         stack.push(hex::decode(&witness[1]).expect("DECODING: FAILED"));
-
-        // IMPLICIT OPCODE EXECUTION (OP_DUP, OP_HASH160, OP_PUSHBYTES_20, OP_EQUALVERIFY, OP_CHECKSIG)
-
-        // OP_DUP
         stack.push(stack.last().cloned().expect("STACK UNDERFLOW"));
-
-        // OP_HASH160
         let pk = stack.pop().expect("STACK UNDERFLOW");
         stack.push(hash160(&pk));
 
-        // EXECUTE INNER REDEEM SCRIPT (OP_PUSHBYTES_20)
         let inner_redeemscript_opcodes: Vec<&str> =
             inner_redeemscript_asm.split_whitespace().collect();
 
@@ -254,7 +223,6 @@ fn script_execution_p2sh(
             }
         }
 
-        // OP_EQUALVERIFY
         let a = stack.pop().expect("STACK UNDERFLOW");
         let b = stack.pop().expect("STACK UNDERFLOW");
 
@@ -262,21 +230,13 @@ fn script_execution_p2sh(
             return Ok(false);
         }
 
-        // OP_CHECKSIG
         script_result = op_checksig(&mut stack, tx.clone(), tx_input_index, input_type)?;
     }
 
     if input_type == "P2SH-P2WSH" {
-        // EXECUTE WITNESS
-
-        // PUSH SIGNATURES
-
         for index in 0..witness.len() - 1 {
             stack.push(hex::decode(&witness[index])?);
         }
-
-        // EXECUTING THE WITNESS SCRIPT
-
         let witness_script_bytes = hex::decode(&witness.last().cloned().expect("SCRIPT MISSING"))?;
 
         let mut index = 0;
@@ -287,11 +247,9 @@ fn script_execution_p2sh(
 
             match opcode {
                 82 => {
-                    // OP_2 SIGNATURE COUNT
                     stack.push(vec![2u8]);
                 }
                 _ if opcode <= 75 as u8 && opcode >= 1 as u8 => {
-                    // OP_PUSHBYTES_33
                     if index + opcode as usize <= witness_script_bytes.len() {
                         let bytes = witness_script_bytes[index..index + opcode as usize].to_vec();
                         stack.push(bytes);
@@ -300,12 +258,10 @@ fn script_execution_p2sh(
                 }
 
                 83 => {
-                    // OP_3 PUBLIC KEY COUNT
                     stack.push(vec![3u8]);
                 }
 
                 174 => {
-                    // OP_CHECKMULTISIG
                     script_result =
                         op_checkmultisig(&mut stack, tx.clone(), tx_input_index, input_type)?;
 
@@ -317,7 +273,6 @@ fn script_execution_p2sh(
                 }
 
                 173 => {
-                    // OP_CHECKSIGVERIFY
                     let result = op_checksig(&mut stack, tx.clone(), tx_input_index, input_type)?;
 
                     if result == true {
@@ -325,8 +280,6 @@ fn script_execution_p2sh(
                     } else {
                         stack.push(vec![0u8]);
                     }
-
-                    // OP_VERIFY
 
                     let top = stack.pop().unwrap();
                     if top == vec![1u8] {
@@ -338,8 +291,6 @@ fn script_execution_p2sh(
                 }
 
                 172 => {
-                    // OP_CHECKSIG
-
                     let sig_length = stack[stack.len() - 1].len();
 
                     if sig_length <= 75 && sig_length >= 70 {
@@ -404,54 +355,3 @@ fn script_execution_p2sh(
     Ok(script_result)
 }
 
-// TO TEST MY CODE DURING DEVELOPMENT
-#[cfg(test)]
-mod test {
-    use std::fs;
-
-    use walkdir::WalkDir;
-
-    use super::*;
-
-    #[test]
-    fn test_script_execution_p2sh() -> Result<()> {
-        let mempool_dir = "./mempool";
-        for entry in WalkDir::new(mempool_dir).into_iter().filter_map(|e| e.ok()) {
-            let path = entry.path();
-            if path.is_file() {
-                match fs::read_to_string(path) {
-                    Ok(contents) => {
-                        match serde_json::from_str::<Transaction>(&contents) {
-                            Ok(transaction) => {
-                                // Check if all inputs' prevout scriptpubkey_type are .p2sh
-                                let all_p2sh = transaction.vin.iter().all(|input| {
-                                    input.prevout.scriptpubkey_type == "p2sh".to_string()
-                                });
-                                if all_p2sh {
-                                    let result = script_execution_p2sh(
-                                        transaction.vin[0].prevout.scriptpubkey_asm.clone(),
-                                        transaction.vin[0]
-                                            .witness
-                                            .clone()
-                                            .unwrap_or(vec!["".to_string()]),
-                                        transaction.vin[0].scriptsig_asm.clone().unwrap(),
-                                        transaction.vin[0].inner_redeemscript_asm.clone().unwrap(),
-                                        transaction,
-                                        0,
-                                    )?;
-
-                                    if result == true {
-                                    } else {
-                                    }
-                                }
-                            }
-                            Err(_e) => {}
-                        }
-                    }
-                    Err(_e) => {}
-                }
-            }
-        }
-        Ok(())
-    }
-}
