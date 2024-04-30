9ddbd9f09c63c9f1052a6be8a2d854eff9416ac1
IamEzio <anshumanmaurya111@gmail.com>
2024-05-01 01:44:58 +0530

    bug fix

---
+++ target/release/deps/code_challenge_2024_IamEzio-7f8f999ea3494096.d
@@ -0,0 +1,16 @@
+/home/anshu/Documents/sob/code-challenge-2024-IamEzio/target/release/deps/code_challenge_2024_IamEzio-7f8f999ea3494096: src/main.rs src/block_mine/mod.rs src/block_mine/block.rs src/block_mine/merkle_root.rs src/block_mine/serialise_tx.rs src/error.rs src/transaction.rs src/validation_checks/mod.rs src/validation_checks/p2pkh.rs src/validation_checks/p2sh.rs src/validation_checks/p2wpkh.rs src/validation_checks/p2wsh.rs
+
+/home/anshu/Documents/sob/code-challenge-2024-IamEzio/target/release/deps/code_challenge_2024_IamEzio-7f8f999ea3494096.d: src/main.rs src/block_mine/mod.rs src/block_mine/block.rs src/block_mine/merkle_root.rs src/block_mine/serialise_tx.rs src/error.rs src/transaction.rs src/validation_checks/mod.rs src/validation_checks/p2pkh.rs src/validation_checks/p2sh.rs src/validation_checks/p2wpkh.rs src/validation_checks/p2wsh.rs
+
+src/main.rs:
+src/block_mine/mod.rs:
+src/block_mine/block.rs:
+src/block_mine/merkle_root.rs:
+src/block_mine/serialise_tx.rs:
+src/error.rs:
+src/transaction.rs:
+src/validation_checks/mod.rs:
+src/validation_checks/p2pkh.rs:
+src/validation_checks/p2sh.rs:
+src/validation_checks/p2wpkh.rs:
+src/validation_checks/p2wsh.rs:
